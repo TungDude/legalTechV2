ca7770b151b831497d265b780867f66f2b2268d1
Tung <krittapop.kms@gmail.com>
2024-02-15 13:16:00 +0700

    update IndexScript

--- static/script/indexScript.js
+++ static/script/indexScript.js
@@ -1,15 +1,12 @@
 async function subPlaneInput() {
-    const container = document.getElementById("subPlaneContainer");
-    const input = document.getElementById("subPlane").value;
+    const subPlaneContainer = document.getElementById("subPlaneContainer");
+    const input = document.getElementById("subPlaneNo").value;
 
-    container.innerHTML = "";
-    container.style.border = "2px solid black";
+    subPlaneContainer.innerHTML = "";
+    // container.style.border = "2px solid black";
     for (let i = 0; i < input; i++) {
-        container.innerHTML += '<label for="subPlane' + i.toString() + '">ขนาดแปลงย่อยที่ ' + (i + 1).toString() + ' ( ไร่)</label><input style="direction: ltr; text-align: right; margin-right: 10px" type="number" id="subPlane' + i.toString() + '" name="subPlane' + i.toString() + '">'
+        subPlaneContainer.innerHTML += '<label for="subPlane' + i.toString() + '">ขนาดแปลงย่อยที่ ' + (i + 1).toString() + ' ( ไร่)</label><input style="direction: ltr; text-align: right; margin-right: 10px" type="number" id="subPlane' + i.toString() + '" name="subPlane' + i.toString() + '">'
     }
-    container.innerHTML += '<br><button style="width: 80px; height: 35px; font-size: 16px;" onclick="redirectPage()">Submit</button'
-}
 
-async function redirectPage() {
-
+    subPlaneContainer.innerHTML += '<br><input style="width: 80px; height: 35px; font-size: 16px;" class="calculateButton" type="submit" value="Submit">'
 }
